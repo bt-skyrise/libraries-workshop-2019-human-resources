d45204fc72eeb865f19f09f37f0d9a99d7e1ac48
Ryszard Tarajkowski <ryszard.tarajkowski@btsignaal.com>
2019-02-21 15:45:47 +0100

    Add project files.

---
+++ Workshop2019.HumanResources/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Workshop2019.HumanResources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
